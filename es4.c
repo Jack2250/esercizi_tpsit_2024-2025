0d3c5acbf7d1fe18b16dd83a061d57dde04d6fc1
Jack2250 <150335369+Jack2250@users.noreply.github.com>
2024-10-02 17:27:23 +0200

    Create es4.c

---
+++ es4.c
@@ -0,0 +1,26 @@
+/*Dato N un numero intero positivo, generare e visualizzare in ordine
+crescente i numeri pari minori o uguali a N.
+*/
+
+//Autore: Redi Giacomo Data: 3/10/2024
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    int N,num=2;
+    
+    do{
+        printf("Inserisci un numero intero positivo\n");
+        scanf("%d",&N);
+    }while(N<=0);
+    
+    printf("I numeri pari minori o uguali a N sono:\n");
+    
+    do{
+        printf("%d\n",num);
+        num = num + 2;
+    }while(num<=N);
+    
+    return 0;
+}
