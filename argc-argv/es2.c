6467531ba7f1d7cfc932917b6e71538138710518
Jack2250 <150335369+Jack2250@users.noreply.github.com>
2024-11-15 18:58:46 +0100

    Update es2.c

--- argc-argv/es2.c
+++ argc-argv/es2.c
@@ -1,4 +1,5 @@
-/*Si scriva un programma che calcoli l’elevazione a potenza del valore intero
+/*
+Si scriva un programma che calcoli l’elevazione a potenza del valore intero
 passato come primo argomento per il secondo argomento intero.
 
 Esempio:
@@ -7,32 +8,33 @@ $./es 2 3
 2 ^ 3 = 8
 */
 
-//Autore:Redi Giacomo Data: 10/10/2024
+// Autore: Redi Giacomo Data: 10/10/2024
 
 #include <stdlib.h>
 #include <stdio.h>
 
-int main(int argc, char *argv [])
+int main(int argc, char *argv[])
 {
-    int n1,n2,potenza=1;
-    
-    if(argc != 3){
-        printf("Errore argomenti\n");
+    int n1, n2, potenza = 1;
+
+    if (argc != 3) {
+        printf("Errore: numero di argomenti errato\n");
         return 0;
     }
-    
-    n1 = atoi(argv [1]);
+
+    n1 = atoi(argv[1]);
     n2 = atoi(argv[2]);
-    
-    if(n1<0 || n2<0){
-        printf("Il numero deve essere maggiore o uguale a 0\n");
+
+    if (n1 < 0 || n2 < 0) {
+        printf("Errore: i numeri devono essere maggiori o uguali a 0\n");
+        return 0;
     }
-    
-    for (int i = 0; i < n2; i++)
-    {
+
+    for (int i = 0; i < n2; i++) {
         potenza = n1 * potenza;
     }
-    
-    printf("La potenza è: %d",potenza);
+
+    printf("%d ^ %d = %d\n", n1, n2, potenza);
+
     return 0;
 }
