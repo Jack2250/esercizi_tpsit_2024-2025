63774e786a57a7d9515b9254201973e111b22430
Jack2250 <150335369+Jack2250@users.noreply.github.com>
2025-02-03 10:16:42 +0200

    Add files via upload

--- Officina/src/Concessionaria.java
+++ Officina/src/Concessionaria.java
@@ -3,16 +3,37 @@ import java.util.ArrayList;
 public class Concessionaria {
     private ArrayList<Veicolo> officina = new ArrayList<>();
 
-    public void addVeicolo(Veicolo v){
+    public void addVeicolo(Veicolo v) {
         officina.add(v);
     }
-    public int numeroVeicoli(){
+
+    public int numeroVeicoli() {
         return officina.size();
     }
 
-    public Veicolo modificaPrezzi(Veicolo v){
-        
+    public void modificaPrezzo(String modello, double prezzo) throws Exception {
+        boolean trovato = false;
+        for (Veicolo v : officina) {
+            if (v.getModello().equalsIgnoreCase(modello)) {
+                v.setPrezzo(prezzo);
+                trovato = true;
+            }
+        }
+        if (!trovato) {
+            throw new Exception();
+        }
+    }
 
-        return ;
+    public void stampaPerPrezzo(double prezzo) throws Exception {
+        boolean trovato = false;
+        for (Veicolo v : officina) {
+            if (prezzo > v.getPrezzo()) {
+                System.out.println(v);
+                trovato = true;
+            }
+        }
+        if (!trovato) {
+            throw new Exception();
+        }
     }
 }
--- Officina/src/Main.java
+++ Officina/src/Main.java
@@ -1,10 +1,36 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
         Concessionaria c = new Concessionaria();
-        Veicolo v1 = new Veicolo("Ford","Fiesta",3000);
+        Veicolo v1 = new Veicolo("Ford", "Fiesta", 3000);
+        Veicolo v2 = new Veicolo("Lambo", "TTTTTTTTTTTTTT", 3000);
         c.addVeicolo(v1);
+        c.addVeicolo(v2);
+
+        try {
+            System.out.println("Inserisci il modello dell'auto da cambiare");
+            String modello = sc.nextLine();
+            System.out.println("Inserisci il nuovo prezzo");
+            double prezzo = sc.nextDouble();
+
+            c.modificaPrezzo(modello, prezzo);
+
+            System.out.println(v1);
+            System.out.println(v2);
+
+            System.out.println("Inserisci il prezzo");
+            double prezzo2 = sc.nextDouble();
+
+            c.stampaPerPrezzo(prezzo2);
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
         System.out.println(v1);
-        System.out.println(c.numeroVeicoli());
+        System.out.println(v2);
     }
 }
