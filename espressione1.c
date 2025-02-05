51223789ec9d423ce10f8c8137d074005d5a8669
Jack2250 <150335369+Jack2250@users.noreply.github.com>
2025-02-05 17:20:34 +0100

    Update espressione1.c

--- espressione1.c
+++ espressione1.c
@@ -27,40 +27,40 @@
 #include <sys/wait.h>
 
 int main() {
-    int p1, p2;
+    int f1, f2;
     int status1, status2;
     
     printf("P: mio PID=%d\n", getpid());
     
-    p1 = fork();
-    if (p1<0) {
+    f1 = fork();
+    if (f1<0) {
         perror("Errore generazione processo F1");
         exit(1);
     }
     
-    if (p1 == 0) {
+    if (f1 == 0) {
         printf("F1: mio PID=%d, mio padre P ha PID=%d\n", getpid(), getppid());
         exit(1 + 2);  // Restituisce il risultato della somma
     }
     
-    printf("P: mio PID=%d, mio figlio F1 ha PID=%d\n", getpid(), p1);
+    printf("P: mio PID=%d, mio figlio F1 ha PID=%d\n", getpid(), f1);
     
-    p2 = fork();
-    if (p2<0) {
+    f2 = fork();
+    if (f2<0) {
         perror("Errore generazione processo F2");
         exit(1);
     }
     
-    if (p2 == 0) {
+    if (f2 == 0) {
         printf("F2: mio PID=%d, mio padre P ha PID=%d\n", getpid(), getppid());
         exit(3 + 4);
     }
     
-    printf("P: mio PID=%d, mio figlio F2 ha PID=%d\n", getpid(), pid_f2);
+    printf("P: mio PID=%d, mio figlio F2 ha PID=%d\n", getpid(), f2);
     
     // Attende la terminazione dei figli e raccoglie i risultati
-    waitpid(p1, &status1, 0);
-    waitpid(p2, &status2, 0);
+    waitpid(f1, &status1, 0);
+    waitpid(f2, &status2, 0);
     
     if (WIFEXITED(status1) && WIFEXITED(status2)) {
         int risultatoF1 = WEXITSTATUS(status1);
